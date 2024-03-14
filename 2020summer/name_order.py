fab9c76509b4a110f34e7ab81fd434adb78f8724
HBAI2024 <163099449+HBAI2024@users.noreply.github.com>
2024-03-14 12:23:25 +0800

    Add files via upload

---
+++ 2020summer/name_order.py
@@ -0,0 +1,32 @@
+name_list = [
+    "LiuQuanying",
+    "WangYueming",
+    "WangYiwen",
+    "WuHaiyan",
+    "ZengAn",
+    "PanDan",
+    "LiuJian",
+    "HuBin",
+    "MiYuanyuan",
+    "GuoDaqing",
+    "DuKai",
+    "KongWanzeng",
+    "PanGang",
+    "DingAmy",
+    "SinhaAniruddha",
+    "WengJuyang",
+    "XuXiaowei",
+    "ZhanYang",
+    "HeYong",
+    "BeckerBenjamin",
+    "QiYu",
+    "ShiYiyu",
+    "WangYalin",
+    "SongXiaowei"
+]
+
+sorted_list = sorted(name_list)
+print(sorted_list)
+
+['BeckerBenjamin', 'DingAmy', 'DuKai', 'GuoDaqing', 'HeYong', 'HuBin', 'KongWanzeng', 'LiuJian', 'LiuQuanying', 'MiYuanyuan', 'PanDan', 'PanGang', 'QiYu', 'ShiYiyu',
+ 'SinhaAniruddha', 'SongXiaowei', 'WangYalin', 'WangYiwen', 'WangYueming', 'WengJuyang', 'WuHaiyan', 'XuXiaowei', 'ZengAn', 'ZhanYang']
